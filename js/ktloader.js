d84260a2411622b080645985cf6cbd8d5fb9f015
reccesoft <87607185+reccesoft@users.noreply.github.com>
2023-04-25 00:53:43 +0200

    done

--- js/ktloader.js
+++ js/ktloader.js
@@ -7,7 +7,16 @@ document.onreadystatechange = function () {
     } else {
         document.querySelector(
             "#loader").style.display = "none";
+
         document.querySelector(
             "body").style.visibility = "visible";
+
+        try {
+            document.querySelector(
+                "#kt-loaderoverlay").style.display = "none";
+        } catch (e) {
+
+        }
+
     }
 };
